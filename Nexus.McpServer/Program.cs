73367e95586924f50e51e59914c489e2b6c13a4e
kevin-cueva <unimagneill@gmail.com>
2026-01-07 08:51:07 -0500

    Config initial for the MCP Server

--- Nexus.McpServer/Program.cs
+++ Nexus.McpServer/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+
+var builder = Host.CreateEmptyApplicationBuilder(null);
+
+//builder.Services.AddMemoryCache();
+
+builder.Services.AddLogging(loggin =>
+{
+    loggin.AddConsole();
+});
+
+//Errores los envie a standard error
+builder.Services.Configure<ConsoleLoggerOptions>(o =>
+{
+    o.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services.AddMcpServer()
+.WithStdioServerTransport()
+.WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
